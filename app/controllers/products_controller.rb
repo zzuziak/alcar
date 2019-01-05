8daee7287b9dd7ea9e1e4cd12e3226ee5fa1b75e
Zuza Zuber <zuberzuzanna@gmail.com>
2019-01-05 12:35:54 +0100

    create crud for categories products and options

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,10 @@
 class ProductsController < ApplicationController
+
+  def index
+    @category = Category.find(params[:category_id])
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
 end
